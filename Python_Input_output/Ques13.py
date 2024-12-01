46cdb22e3a8f33f6e5d591393a3534b1dbb9602c
ABHAY KUMAR <in.mail.abhay@gmail.com>
2024-12-01 20:00:38 +0530

    Ques13.py
    Update logic in this program

--- Python_Input_output/Ques13.py
+++ Python_Input_output/Ques13.py
@@ -6,4 +6,14 @@ if 10000<=num<=99999:
         rem=num%10
         new_num=new_num*10+((rem+1))
         num//=10
-    print('New number= ',new_num)
+    print('Reverse number= ',new_num,end='\n')
+    
+    after_rev=0
+    while new_num>0:
+        number=new_num%10
+        after_rev=after_rev*10+number
+        new_num=new_num//10
+    print("After adding 1 will be=",after_rev)
+else:
+    print("Please Enter number between 10000 to 99999")
+    
