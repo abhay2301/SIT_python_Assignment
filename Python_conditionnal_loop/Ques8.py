b4072324be8ec52276ce4ddd148b49775c6ae452
ABHAY KUMAR <in.mail.abhay@gmail.com>
2024-12-11 20:20:06 +0530

    Update my mistake

--- Python_conditionnal_loop/Ques8.py
+++ Python_conditionnal_loop/Ques8.py
@@ -3,8 +3,8 @@ The Absolute value= 125.0'''
 
 number=float(input('Enter a number: '))
 if number>0:               # for check number is positive or not
-    absolute_value= number
+    absolute_value= number//1
 else:
-    absolute_value= -number
+    absolute_value= -number//1
 
 print('The Absolute value=',absolute_value) 
